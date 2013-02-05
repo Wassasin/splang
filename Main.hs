b7d85fee1e1293d80da01c9125903a3673bf0b23
Joshua Moerman <lakseru@gmail.com>
2013-02-05 15:40:07 +0100

    Merged this shit and created a new nice main

--- Main.hs
+++ Main.hs
@@ -1,10 +1,21 @@
 import System.Environment
-import Lexer
+import qualified Lexer
+import Parser
+import Text.Parsec
+
 
 main :: IO ()
 main = do
 	[file] <- getArgs
 	s <- readFile file
-	case lexer (s, 0) of
-		Match xs _ -> print $ xs
-		NoMatch n -> print $ n
+	putStrLn "File:"
+	putStrLn s
+	putStrLn "Lexing result:"
+	case Lexer.lexer (s, 0) of
+		Lexer.Match xs _ -> do
+			print xs
+			putStrLn "Parsing result:"
+			case (parse parseTerm4 file xs) of
+				Right x -> print x
+				Left pError -> print pError
+		Lexer.NoMatch lError -> print lError
