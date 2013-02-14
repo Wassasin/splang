b048e322d7b075b822e62363c56a7e8cbf2023c4
Wouter Geraedts <w.geraedts@onii.org>
2013-02-14 14:39:23 +0100

    ParserLib: fixed errorhandling

--- ParserLib.hs
+++ ParserLib.hs
@@ -10,8 +10,8 @@ data Error = Unexpected Token | EndOfStream | Ambiguity
 	deriving (Show, Eq)
 type ParseInput = (Source.IndexSpan, [Token])
 
-data ParseResult a = Match [(a, ParseInput)] | NoMatch Error
-type ParseFunc a = ParseInput -> ParseResult a
+data ParseResult a = Match [(a, ParseInput)] | NoMatch
+type ParseFunc a = ParseInput -> (ParseResult a, Maybe Error)
 data ParseFuncD a = PF (ParseFunc a)
 
 bo :: ParseFuncD a -> ParseFunc a
@@ -22,15 +22,15 @@ mo f = PF f
 
 parse :: ParseFuncD a -> [Token] -> Either [a] Error
 parse f is = (case (bo f) (Source.IndexSpan 0 0, is) of
-		NoMatch e	-> Right e
-		Match xs	-> (case dualmap (\ i -> (case i of
-					(x, (_, []))	-> Left x	-- Completely parsed, possible result
-					(_, (_, t:_))	-> Right t	-- Tokens left to parse
-				)) xs of
-					([], ts)	-> Right (foldr1 bestNoMatch (map (\ t -> Unexpected t) ts)) -- If no options, show last token where parsing terminated
-					(xs, _)		-> Left xs	-- Return all possible options (ideally one option)
-			)
-	)
+	(NoMatch, Just e)	-> Right e
+	(Match xs, pe)		-> (case dualmap (\ i -> (case i of
+			(x, (_, []))	-> Left x	-- Completely parsed, possible result
+			(_, (_, t:_))	-> Right t	-- Tokens left to parse
+		)) xs of
+			([], ts)	-> Right (case bestMError pe (Just (foldr1 bestError (map (\ t -> Unexpected t) ts))) of -- If no options, show last token where parsing terminated
+						Just e	-> e)
+			(xs, _)		-> Left xs	-- Return all possible options (ideally one option)
+		))
 
 dualmap :: (a -> Either b c) -> [a] -> ([b], [c])
 dualmap f xs = dualmapr f xs ([], [])
@@ -41,20 +41,27 @@ dualmap f xs = dualmapr f xs ([], [])
 			Left b	-> dualmapr f xs (b:bs, cs)
 			Right c	-> dualmapr f xs (bs, c:cs)
 
-bestNoMatch :: Error -> Error -> Error
-bestNoMatch EndOfStream _ = EndOfStream
-bestNoMatch _ EndOfStream = EndOfStream
-bestNoMatch (Unexpected (Lexer.Token x (Source.IndexSpan xx xy))) (Unexpected (Lexer.Token y (Source.IndexSpan yx yy)))
+bestError :: Error -> Error -> Error
+bestError EndOfStream _ = EndOfStream
+bestError _ EndOfStream = EndOfStream
+bestError (Unexpected (Lexer.Token x (Source.IndexSpan xx xy))) (Unexpected (Lexer.Token y (Source.IndexSpan yx yy)))
 	| xy <= yy	= Unexpected (Lexer.Token y (Source.IndexSpan yx yy))
 	| otherwise	= Unexpected (Lexer.Token x (Source.IndexSpan xx xy))
 
+bestMError :: Maybe Error -> Maybe Error -> Maybe Error
+bestMError (Just x) (Just y)	= Just (bestError x y)
+bestMError (Just x) Nothing	= Just x
+bestMError Nothing (Just y)	= Just y
+bestMError Nothing Nothing	= Nothing
+
 (<|>) :: ParseFuncD a -> ParseFuncD a -> ParseFuncD a
 (<|>) fd gd = mo (\ i -> case ((bo fd) i, (bo gd) i) of
-		(Match xs, Match ys)	-> Match (xs ++ ys)
-		(Match xs, NoMatch _)	-> Match xs
-		(NoMatch _, Match ys)	-> Match ys
-		(NoMatch t, NoMatch u)	-> NoMatch (bestNoMatch t u)
-	)
+		((xr, xe), (yr, ye)) -> case bestMError xe ye of
+			e -> (case (xr, yr) of
+				(Match xs, Match ys)	-> (Match (xs ++ ys), e)
+				(Match xs, NoMatch)	-> (Match xs, e)
+				(NoMatch, Match ys)	-> (Match ys, e)
+				(NoMatch, NoMatch)	-> (NoMatch, e)))
 
 many1 :: ParseFuncD a -> ParseFuncD [a]
 many1 fd = do
@@ -68,48 +75,46 @@ many fd = do
 instance Monad ParseFuncD where
 --	(>>=) :: ParseFuncD a -> (a -> ParseFuncD b) -> ParseFuncD b
 	(>>=) fd gm = mo (\ (l0, xs) -> case (bo fd) (l0, xs) of
-			NoMatch e -> NoMatch e
-			Match xms -> case dualmap (
+			(NoMatch, Just e) -> (NoMatch, Just e)
+			(Match xms, pe0) -> case dualmap (
 					\(a, (l1, ys)) -> case bo (gm a) (l1, ys) of
-						NoMatch e -> Left e
-						Match yms -> Right (map (\ (b, (l2, zs)) -> (b, (foldr1 Source.merge [l0, l1, l2], zs))) yms)
+						(NoMatch, Just e1)	-> Left (bestMError pe0 (Just e1))
+						(Match yms, pe1)	-> Right (map (\ (b, (l2, zs)) -> (b, (foldr1 Source.merge [l0, l1, l2], zs))) yms, bestMError pe0 pe1)
 				) xms of
-					(errors, []) -> NoMatch (foldr1 bestNoMatch errors)
-					(_, matchesSet) -> Match (concat matchesSet)
-		)
+					(errors, []) -> (NoMatch, foldr1 bestMError errors)
+					(_, metup) -> case unzip metup of
+						(matches, pe2s) -> (Match (concat matches), foldr1 bestMError pe2s))
 
 --	return :: a -> ParseFuncD a
-	return a = mo (\ i -> Match [(a, i)])
+	return a = mo (\ i -> (Match [(a, i)], Nothing))
 
 produce :: (Source.IndexSpan -> a) -> ParseFuncD a
-produce f = mo (\ (l, xs) -> Match [(f l, (l, xs))])
+produce f = mo (\ (l, xs) -> (Match [(f l, (l, xs))], Nothing))
 
 produceP1 :: (P1Meta -> a) -> ParseFuncD a
 produceP1 f = produce (f . constructP1)
 
 newObject :: ParseFuncD a -> ParseFuncD a
 newObject fd = mo (\ (Source.IndexSpan from to, is) -> case (bo fd) (Source.IndexSpan to to, is) of
-		NoMatch e	-> NoMatch e
-		Match xs	-> Match (map (\output -> case output of
+		(NoMatch, Just e)	-> (NoMatch, Just e)
+		(Match xs, pe0)		-> case map (\output -> case output of
 				(x, (Source.IndexSpan _ rto, os)) -> (x, (Source.IndexSpan from rto, os))
-			) xs)
-	)
+			) xs of
+				matches -> (Match matches, pe0))
 
 parseToken :: (Lexer.TokenE -> Bool) -> ParseFuncD Lexer.TokenE
 parseToken f = mo (\ i -> case i of
-		(_, [])				-> NoMatch EndOfStream
+		(_, [])				-> (NoMatch, Just EndOfStream)
 		(il, (Lexer.Token t l:xs))	-> case f t of
-			True -> Match [(t, (Source.merge il l, xs))]
-			False -> NoMatch (Unexpected (Lexer.Token t l))
-	)
+			True -> (Match [(t, (Source.merge il l, xs))], Nothing)
+			False -> (NoMatch, Just (Unexpected (Lexer.Token t l))))
 	
 equalsToken :: Lexer.TokenE -> ParseFuncD Lexer.TokenE
 equalsToken t = parseToken ((==) t)
 
 parseOne :: (Token -> Maybe a) -> ParseFuncD a
 parseOne f = mo (\ i -> case i of
-		(_, [])				-> NoMatch EndOfStream
+		(_, [])				-> (NoMatch, Just EndOfStream)
 		(il, (Lexer.Token t l):xs)	-> case f (Lexer.Token t l) of
-			Nothing -> NoMatch (Unexpected (Lexer.Token t l))
-			Just result -> Match [(result, (Source.merge il l, xs))]
-	)
+			Nothing -> (NoMatch, Just (Unexpected (Lexer.Token t l)))
+			Just result -> (Match [(result, (Source.merge il l, xs))], Nothing))
