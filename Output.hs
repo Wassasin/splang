3e1e1a0cb15ed07cac3c33ad047218269e05ee6a
Joshua Moerman <lakseru@gmail.com>
2013-02-15 16:27:26 +0100

    MOAR COLORS! Functions are now green.

--- Output.hs
+++ Output.hs
@@ -6,6 +6,7 @@ data Styles = Type
 	| Variable
 	| Constant
 	| Keyword
+	| Function
 
 data OpenClose a = Open a | Close a
 type Markup a = Either Char (OpenClose a)
@@ -28,6 +29,7 @@ close a = return $ Right $ Close a
 keyword str = open Keyword ++ lift str ++ close Keyword
 constant str = open Constant ++ lift str ++ close Constant
 variable str = open Variable ++ lift str ++ close Variable
+function str = open Function ++ lift str ++ close Function
 
 join :: (a -> [b]) -> [b] -> [a] -> [b]
 join f s [] = []
@@ -46,7 +48,7 @@ outputProgram (Program pr _) = join (outputDecl 0) (lift "\n\n") pr
 
 outputDecl :: Int -> Decl a -> MarkupString Styles
 outputDecl n (VarDecl t i e _) = tabs n ++ outputType t ++ lift " " ++ variable i ++ lift " = " ++ outputExpr False e ++ lift ";"
-outputDecl n (FunDecl t i args vdecls stmts _) = tabs n ++ outputType t ++ lift " " ++ lift i ++ lift "(" ++ join outputArg (lift ", ") args ++ lift "){\n" ++ delim (outputDecl (n+1)) (lift "\n") vdecls ++ delim (outputStmt (n+1)) (lift "\n") stmts ++ tabs n ++ lift "}"
+outputDecl n (FunDecl t i args vdecls stmts _) = tabs n ++ outputType t ++ lift " " ++ function i ++ lift "(" ++ join outputArg (lift ", ") args ++ lift "){\n" ++ delim (outputDecl (n+1)) (lift "\n") vdecls ++ delim (outputStmt (n+1)) (lift "\n") stmts ++ tabs n ++ lift "}"
 
 outputArg :: (Type a, Identifier) -> MarkupString Styles
 outputArg (t, i) = outputType t ++ lift " " ++ variable i
@@ -94,7 +96,7 @@ outputExpr b (Binop e1 bop e2 _)	= enclose b $ outputExpr True e1 ++ outputBinar
 outputExpr b (Unop uop e _)			= enclose b $ outputUnaryOperator uop ++ outputExpr True e
 outputExpr _ (Kint n _)				= constant (show n)
 outputExpr _ (Kbool b _)			= constant (show b)
-outputExpr _ (FunCall i exprs _)	= lift i ++ lift "(" ++ join (outputExpr False) (lift ", ") exprs ++ lift ")"
+outputExpr _ (FunCall i exprs _)	= function i ++ lift "(" ++ join (outputExpr False) (lift ", ") exprs ++ lift ")"
 outputExpr _ (Pair e1 e2 _)			= lift "(" ++ outputExpr False e1 ++ lift ", " ++ outputExpr False e2 ++ lift ")"
 outputExpr _ (List exprs _)			= lift "[" ++ join (outputExpr False) (lift ", ") exprs ++ lift "]"
 
