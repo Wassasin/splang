92117a2224bdc420770987f8bd3120a07c223cee
Wouter Geraedts <git@woutergeraedts.nl>
2013-03-25 20:54:57 +0100

    Typing: completed InferMonad

--- Typing.hs
+++ Typing.hs
@@ -7,24 +7,24 @@ import Meta (ASTMeta, getMeta)
 import qualified AST
 
 type FTid = Int
-data FreeType a = FT FTid a
+data FreeType m = FT FTid m
 	deriving (Show, Read)
 	
-data PolyType a = Poly (FreeType a) (PolyType a) a | Mono (MonoType a) a
+data PolyType m = Poly (FreeType m) (PolyType m) m | Mono (MonoType m) m
 	deriving (Show, Read)
 
-data MonoType a = Func [MonoType a] (MonoType a) a
-	| Pair (MonoType a) (MonoType a) a
-	| List (MonoType a) a
-	| Free (FreeType a) a
-	| Int a
-	| Bool a
+data MonoType m = Func [MonoType m] (MonoType m) m
+	| Pair (MonoType m) (MonoType m) m
+	| List (MonoType m) m
+	| Free (FreeType m) m
+	| Int m
+	| Bool m
 	deriving (Show, Read)
 	
-instance Eq (FreeType a) where
+instance Eq (FreeType m) where
 	(==) (FT x _) (FT y _) = x == y
 
-instance Eq (MonoType a) where
+instance Eq (MonoType m) where
 	(==) (Func xs xr _) (Func ys yr _)	= xs == ys && xr == yr
 	(==) (Pair xx xy _) (Pair yx yy _)	= xx == yx && yx == yy
 	(==) (List x _) (List y _)		= x == y
@@ -33,50 +33,66 @@ instance Eq (MonoType a) where
 	(==) (Bool _) (Bool _)			= True
 	(==) _ _				= False
 	
-type Substitution a = MonoType a -> MonoType a
-
-data Unification a = Success (Substitution a) | Fail (MonoType a) (MonoType a)
+type Substitution m = MonoType m -> MonoType m
+data Unification m = Success (Substitution m) | Fail (MonoType m) (MonoType m)
 
 type InferState = FTid
-type InferContext a = [(AST.IdentID, MonoType a)]
+type InferContext m = [(AST.IdentID, MonoType m)]
 
-data InferError a = CannotUnify (MonoType a) (MonoType a)
-data InferWarning a = Void
-type InferResult a b = ErrorContainer (InferError b) (InferWarning b) (a, InferState)
+data InferError m = CannotUnify (MonoType m) (MonoType m)
+data InferWarning m = Void
+type InferResult m a = ErrorContainer (InferError m) (InferWarning m) (a, InferState)
 
-type InferMonad a b = InferState -> InferResult a b
-type InferFunc a b = InferContext b -> a -> MonoType b -> InferMonad (Substitution b) b
+type InferMonad m a = InferState -> InferResult m a
+data InferMonadD m a = IM (InferMonad m a)
+type InferFunc m a = InferContext m -> a -> MonoType m -> InferMonadD m (Substitution m)
 
-substitute :: MonoType a -> MonoType a -> Substitution a
+bo :: InferMonadD m a -> InferMonad m a
+bo (IM f) = f
+
+mo :: InferMonad m a -> InferMonadD m a
+mo f = IM f
+
+instance Monad (InferMonadD m) where
+	-- (>>=) :: InferMonadD m a -> (InferResult m a -> InferMonadD m b) -> InferMonadD m b
+	(>>=) fd gm = mo $ \st1 -> do
+		(a, st2) <- bo fd st1
+		(b, st3) <- bo (gm a) st2
+		return (b, st3)
+	
+	-- return :: a -> InferMonadD m a
+	return a = mo $ \st -> return (a, st) 
+
+substitute :: MonoType m -> MonoType m -> Substitution m
 substitute x y z
 	| x == z	= y
 	| otherwise	= z
 
-compose :: Unification a -> Unification a -> Unification a
+compose :: Unification m -> Unification m -> Unification m
 compose (Success x) (Success y)	= Success (x . y)
 compose (Fail x y) _		= Fail x y
 compose _ (Fail x y)		= Fail x y
 
-ftvm :: MonoType a -> [FreeType a]
+ftvm :: MonoType m -> [FreeType m]
 ftvm (Free a _)		= [a]
 ftvm (Func ts t _)	= foldr (union . ftvm) (ftvm t) ts
 ftvm (Pair t1 t2 _)	= union (ftvm t1) (ftvm t2)
 ftvm (List t _)		= ftvm t
 ftvm _			= []
 
-ftv :: PolyType a -> [FreeType a]
+ftv :: PolyType m -> [FreeType m]
 ftv (Mono t _) = ftvm t
 ftv (Poly a t _) = filter ((/=) a) (ftv t)
 
-mgu :: MonoType a -> MonoType a -> Unification a
+mgu :: MonoType m -> MonoType m -> Unification m
 mgu (Free _ _) (Free _ _)		= Success id
 mgu (Free a al) t
 	| elem a (ftvm t)		= Fail (Free a al) t
 	| otherwise			= Success (substitute (Free a al) t)
 mgu t (Free b bl)			= mgu (Free b bl) t
-mgu (Func xs xr _) (Func ys yr _)	= foldr (\ (x, y) su -> case su of
-						Success s	-> compose (mgu (s x) (s y)) su
-						Fail x y	-> Fail x y
+mgu (Func xs xr _) (Func ys yr _)	= foldr (\(x, y) su -> case su of
+							Success s	-> compose (mgu (s x) (s y)) su
+							Fail x y	-> Fail x y
 						) (mgu xr yr) (zip xs ys)
 mgu (Pair xx xy _) (Pair yx yy _)	= case mgu xy yy of
 						Success s	-> mgu (s xx) (s yx)
@@ -86,23 +102,23 @@ mgu (Int _) (Int _)			= Success id
 mgu (Bool _) (Bool _)			= Success id
 mgu x y					= Fail x y
 
-apply :: InferContext a -> Substitution a -> InferContext a
+apply :: InferContext m -> Substitution m -> InferContext m
 apply c s = map (\(i, t) -> (i, s t)) c
 
-genMgu :: MonoType a -> MonoType a -> InferMonad (Substitution a) a
-genMgu t1 t2 = \st -> case mgu t1 t2 of
+genMgu :: MonoType m -> MonoType m -> InferMonadD m (Substitution m)
+genMgu t1 t2 = mo $ \st -> case mgu t1 t2 of
 	Fail u1 u2 -> returnFatal $ CannotUnify u1 u2
 	Success s -> return (s, st)
 
-genFresh :: ASTMeta a => a b -> InferMonad (MonoType b) b
-genFresh x = let m = getMeta x in \st -> return (Free (FT st m) m, st+1)
+genFresh :: ASTMeta a => a m -> InferMonadD m (MonoType m)
+genFresh x = let m = getMeta x in mo $ \st -> return (Free (FT st m) m, st+1)
 
-inferExpr :: InferFunc (P2 AST.Expr) P2Meta
-inferExpr c t (Pair e1 e2 m) = do
-	a1 <- genFresh (getMeta e1)
+inferExpr :: InferFunc P2Meta (P2 AST.Expr)
+inferExpr c (AST.Pair e1 e2 m) t = do
+	a1 <- genFresh e1
 	s1 <- inferExpr c e1 a1
-	a2 <- genFresh
+	a2 <- genFresh e2
 	s2 <- inferExpr (apply c s1) e2 a2
 	let s3 = s2 . s1
-	s4 <- genMgu (s3 t) (s3 $ Pair a1 a2)
+	s4 <- genMgu (s3 t) (s3 $ Pair a1 a2 m)
 	return $ s4 . s3
