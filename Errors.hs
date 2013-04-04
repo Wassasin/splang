ed19be1e3ff52772310575c67399bcb80769f432
Joshua Moerman <lakseru@gmail.com>
2013-04-04 13:49:09 +0200

    Errors: possibility to add error or warning, without returning, without failing :)

--- Errors.hs
+++ Errors.hs
@@ -1,4 +1,4 @@
-module Errors (ErrorContainer(..), returnWithWarning, returnWithError, returnFatal) where
+module Errors (ErrorContainer(..), addWarning, returnWithWarning, addError, returnWithError, returnFatal) where
 
 -- errors, warnings and data
 data ErrorContainer e w d = Result d [e] [w] | FatalError e [e] [w]
@@ -11,8 +11,14 @@ instance Monad (ErrorContainer e w) where
 		FatalError ferror errors2 warnings2 -> FatalError ferror (errors ++ errors2) (warnings ++ warnings2)
 	(>>=) (FatalError fe es ws) f = FatalError fe es ws
 
+addWarning :: w -> ErrorContainer e w ()
+addWarning w = Result () [] [w]
+
 returnWithWarning :: d -> w -> ErrorContainer e w d
 returnWithWarning x w = Result x [] [w]
+
+addError :: e -> ErrorContainer e w ()
+addError e = Result () [e] []
 
 returnWithError :: d -> e -> ErrorContainer e w d
 returnWithError x e = Result x [e] []
