1d43d99abc0e10fe3d63bcdeb978cf5f562faa5c
Joshua Moerman <lakseru@gmail.com>
2013-06-14 12:58:49 +0200

    Tests: Added test/demo for extern spl :D

---
+++ tests/llvm/externspl/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+splang --target=llvm mergesort.spl | opt -O3 | llc -filetype=obj > mergesort.o
+splang --target=llvm insertionsort.spl | opt -O3 | llc -filetype=obj > insertionsort.o
+splang --target=llvm main.spl | opt -O3 | llc -filetype=obj > main.o
+
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   ld -native shuffle.o get_int.o
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   ld /usr/lib/libc.dylib /usr/lib/crt1.o mergesort.o insertionsort.o main.o
+fi
+
+./a.out
