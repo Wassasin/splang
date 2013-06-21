f045c760f78ee435d972e6df7d980c64f0188180
Wouter Geraedts <git@woutergeraedts.nl>
2013-06-21 23:25:33 +0200

    tests/llvm/ccp: fixed linux run

--- tests/llvm/cpp/run.sh
+++ tests/llvm/cpp/run.sh
@@ -5,7 +5,7 @@ clang++ -std=c++11 -c -o main.o main.cpp
 
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
-   ld -native shuffle.o get_int.o
+   llvm-ld -native sum.o main.o /usr/lib/x86_64-linux-gnu/libstdc++.so.6
 elif [[ "$unamestr" == 'Darwin' ]]; then
    ld /usr/lib/libc.dylib /usr/lib/libstdc++.dylib /usr/lib/crt1.o sum.o main.o
 fi
